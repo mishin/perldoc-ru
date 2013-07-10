495aa87d72e85accccef6d5b6a73ee915ea36f2b
Nikolay Mishin <mi@ya.ru>
2013-07-10 19:44:21 +0400

    Update pod_path.pl

--- tools/pod_path.pl
+++ tools/pod_path.pl
@@ -1,5 +1,22 @@
+use Modern::Perl;
 use File::Spec::Functions qw(catdir splitdir);
-my @perl_exe_path =splitdir($^X);
-my @head_perl_path = @perl_exe_path[0..$#perl_exe_path-2];
-my $pod_path=catdir(@head_perl_path,'lib','pods');
-print $pod_path;
+use DDP;
+my $cmd = q{perldoc -l perldoc};
+use IPC::Open3;
+my @pod_path        = splitdir( run_shell($cmd) );
+my @pod_path_splice = @pod_path[ 0 .. $#pod_path - 1 ];
+my $pod_path        = catdir(@pod_path_splice);
+say $pod_path;
+
+sub run_shell {
+    my $cmd = shift;
+    my ( $HIS_IN, $HIS_OUT, $HIS_ERR ) = ( q{}, q{}, q{} );
+    my $pid = eval { open3( $HIS_IN, $HIS_OUT, $HIS_ERR, $cmd, () ); };
+    if ($@) {
+        warn($@);
+    }
+    waitpid( $pid, 0 );
+    my $child_exit_status = $? >> 8;
+    my $path              = <$HIS_OUT>;
+    return $path;
+}
