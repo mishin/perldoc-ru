78fbacf9f38878c970e69304dde321d508c3beb0
Nikolay Mishin <mi@ya.ru>
2013-07-26 22:27:50 +0400

    remove comment

--- tools/configure_git.pl
+++ tools/configure_git.pl
@@ -11,25 +11,13 @@ use File::Spec::Functions qw(catdir catfile);
 
 say "Configuration for global work using git";
 
-# print 'Please write your Github user name: ';
 my $user = prompt( 'Please write your Github user name:', getlogin() );
 chomp $user;
-# say $user;
-
-# my $user = readline;
 
 
 my $email = prompt( 'Please write your Github email address:', '' );
 chomp $email;
-# say $email;
-my $home = home();
-# my $home     = File::HomeDir->my_home;
- # my $work_path = File::HomeDir->my_documents;
-# say $home;
 
-# print 'Please write your Github email address: ';
-# my $email = readline;
-# 
 
 system('git', 'config', '--global', 'user.name',	$user		);
 system('git', 'config', '--global', 'user.email',	$email		);
@@ -38,5 +26,5 @@ system('git', 'config', '--global', 'pack.threads',	0		);
 system('git', 'config', '--global', 'core.autocrlf',	'false'		);
 system('git', 'config', '--global', 'apply.whitespace',	'nowarn'	);
 system('git', 'config', '--global', 'color.ui',		'auto'		);
-system('git', 'config', '--global', 'core.excludesfile',catfile($home,'.gitignore'));
+system('git', 'config', '--global', 'core.excludesfile',catfile(home(),'.gitignore'));
 system('git', 'config', '--global', 'alias.up',		'pull --rebase'	);
