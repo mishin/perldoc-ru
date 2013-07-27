00487bc00be06c3254f846594291209d9c0b5195
Nikolay Mishin <mi@ya.ru>
2013-07-28 01:47:48 +0400

    russian language trouble

--- pod2-ru/POD2-RU/Build.bat
+++ pod2-ru/POD2-RU/Build.bat
@@ -24,7 +24,7 @@ sub magic_number_matches {
   open FH, '_build\\magicnum' or return 0;
   my $filenum = <FH>;
   close FH;
-  return $filenum == 451110;
+  return $filenum == 123321;
 }
 
 my $progname;
--- pod2-ru/POD2-RU/lib/POD2/RU.pm
+++ pod2-ru/POD2-RU/lib/POD2/RU.pm
@@ -5,7 +5,7 @@ use warnings;
 use base 'Exporter';
 use base 'POD2::Base';
 
-our $VERSION = '5.18.0.1.04';
+our $VERSION = '5.18.0.1.05';
 
 our @EXPORT = qw(print_pod print_pods);
 
